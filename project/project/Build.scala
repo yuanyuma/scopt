e60714039db86d24884b5ee8247cb1fbbb9b6a93
toshiyuki <t.toshi.0412@gmail.com>
2013-03-08 22:47:42 +0900

    Changed the repository url of sbt-site-plugin.

--- project/project/Build.scala
+++ project/project/Build.scala
@@ -3,5 +3,5 @@ object PluginDef extends Build {
   override def projects = Seq(root)
   lazy val root = Project("plugins", file(".")) dependsOn(ghpages, site)
   lazy val ghpages = uri("git://github.com/jsuereth/xsbt-ghpages-plugin.git#1e7611")
-  lazy val site = uri("git://github.com/sbt/sbt-site-plugin.git#268967")
+  lazy val site = uri("git://github.com/sbt/sbt-site.git#268967")
 }
