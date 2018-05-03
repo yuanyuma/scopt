665948bee0a052f8f44baafca87788136412bb33
xuwei-k <6b656e6a69@gmail.com>
2018-05-03 22:59:43 +0900

    Scala 2.12.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
 object Dependencies {
   lazy val scala210 = "2.10.7"
   lazy val scala211 = "2.11.12"
-  lazy val scala212 = "2.12.4"
+  lazy val scala212 = "2.12.6"
   lazy val scala213 = "2.13.0-M3"
 
   lazy val specs2 = Def.setting {
