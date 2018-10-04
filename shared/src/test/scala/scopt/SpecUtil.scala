f4d533e21bcefe715172036b731242d1bffb9570
xuwei-k <6b656e6a69@gmail.com>
2018-10-05 00:24:09 +0900

    add jdk11 test

--- shared/src/test/scala/scopt/SpecUtil.scala
+++ shared/src/test/scala/scopt/SpecUtil.scala
@@ -3,6 +3,6 @@ import SpecUtilPlatform._
 object SpecUtil {
   implicit class RichString(self: String) {
     def newline: String = self + envLineSeparator
-    def newlines: String = self.lines.mkString(envLineSeparator)
+    def newlines: String = augmentString(self).lines.mkString(envLineSeparator)
   }
 }
