b17bf156234789e8ab9d561e508e9a28d618d3a6
xuwei-k <6b656e6a69@gmail.com>
2019-01-05 17:43:14 +0900

    remove unnecessary scala-parser-combinators deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,19 +1,6 @@
-import sbt._
-import Keys._
-import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
-
 object Dependencies {
   lazy val scala210 = "2.10.7"
   lazy val scala211 = "2.11.12"
   lazy val scala212 = "2.12.8"
   lazy val scala213 = "2.13.0-M5"
-
-  lazy val parserCombinators = Def.setting {
-    CrossVersion.partialVersion(scalaVersion.value) match {
-      case Some((2, 10 | 13)) =>
-        Nil
-      case _ =>
-        Seq("org.scala-lang.modules" %%% "scala-parser-combinators" % "1.1.0" % Test)
-    }
-  }
 }
