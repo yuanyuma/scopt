5faf0e3d5b2f5f5fa2e4be90db012fa5206218e7
Christophe Vidal <kriss@krizalys.com>
2019-06-23 09:08:30 +0700

    Updated Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ object Dependencies {
   lazy val scala210 = "2.10.7"
   lazy val scala211 = "2.11.12"
   lazy val scala212 = "2.12.8"
-  lazy val scala213 = "2.13.0-M5"
+  lazy val scala213 = "2.13.0"
 }
