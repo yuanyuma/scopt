095c65fa581d2afe31954d3e9a6097179169a6c5
Timothy Klim <git@timothyklim.com>
2021-04-27 19:44:58 +0700

    Upgrade to Scala 3.0.0-RC3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ object Dependencies {
   lazy val scala210 = "2.10.7"
   lazy val scala211 = "2.11.12"
   lazy val scala212 = "2.12.13"
-  lazy val scala213 = "2.13.4"
-  lazy val scala3 = "3.0.0-RC2"
+  lazy val scala213 = "2.13.5"
+  lazy val scala3 = "3.0.0-RC3"
   val verifyVersion = "1.0.0"
 }
